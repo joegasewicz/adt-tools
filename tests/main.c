912444fa8cd400ac3625b2c15c77737a31dc9b6b
joegasewicz <joegasewicz@gmail.com>
2022-11-22 19:06:02 +0000

    tests passing

--- tests/main.c
+++ tests/main.c
@@ -1,22 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <check.h>
-#include <math.h>
 
-
-
-
-START_TEST(joe_test)
+int hello()
 {
 
-    ck_assert_str_eq("hello", "goodbye");
+    return 1;
+}
+START_TEST (test_CHECK)
+{
+  /* unit test code */
+  ck_assert_int_eq(2, 2);
 }
 END_TEST
-// /home/joe/Codeblocks/check/src/check.c
 
 int main()
 {
+    TCase *tc_main;
+    Suite *s;
+    SRunner *sr;
+    int number_failed;
 
-    printf("Hello world!\n");
-    return 0;
+    s = suite_create("ADTToolsSuite");
+    tc_main = tcase_create("Main");
+    tcase_add_test(tc_main, test_CHECK);
+    suite_add_tcase(s, tc_main);
+    sr = srunner_create(s);
+    srunner_run_all(sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed(sr);
+    srunner_free(sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
